432f8a579c05d8cb3f961e75cfaaa1ff2cab87b5
Brombin Andrey <arcadka@gmail.com>
2025-06-01 00:25:14 +0300

    init commit

---
+++ src/main/java/ru/brombin/assistant_ai_outbox_processor/AssistantAiOutboxProcessorApplication.java
@@ -0,0 +1,13 @@
+package ru.brombin.assistant_ai_outbox_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantAiOutboxProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantAiOutboxProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/brombin/assistant_ai_outbox_processor/AssistantAiOutboxProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.brombin.assistant_ai_outbox_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantAiOutboxProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
